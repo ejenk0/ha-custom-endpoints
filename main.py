4fcae2ae56f0f619615c997eec0eb53f35986105
Evyn Jenkins <74844913+ejenk0@users.noreply.github.com>
2025-07-30 13:51:50 +1000

    Test CI

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route("/ping", methods=["GET"])
 def ping():
-    return jsonify({"message": "pong"})
+    return jsonify({"message": "pong", "date": datetime.now().isoformat()})
 
 
 if __name__ == "__main__":
